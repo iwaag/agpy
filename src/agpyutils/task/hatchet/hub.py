a12db10860befff4648e0b9da1da9762ef02175c
iwaag <eiji-of-empires@nifty.com>
2026-03-16 00:25:42 +0900

    add metadata

--- src/agpyutils/task/hatchet/hub.py
+++ src/agpyutils/task/hatchet/hub.py
@@ -4,7 +4,7 @@ from typing import override
 from agpyutils.task.hub import TaskHub
 import agpyutils.task.models as models
 import agpyutils.task.hatchet.workflows.basic as workflows_basic
-from hatchet_sdk import Hatchet
+from hatchet_sdk import Hatchet, TriggerWorkflowOptions
 import asyncio
 
 class TaskHub_Hatchet(TaskHub):
@@ -13,5 +13,5 @@ class TaskHub_Hatchet(TaskHub):
 
     @override
     def request_unmanaged_labor(self, task: models.Task_UnmanagedLabor):
-        workflows_basic.task_unmanaged_labor.run_no_wait(input=task)
+        workflows_basic.task_unmanaged_labor.run_no_wait(input=task, options=TriggerWorkflowOptions(additional_metadata={"unmanaged_labor_user_id": task.meta.user_id}))
 
