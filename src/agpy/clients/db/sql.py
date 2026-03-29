b5a12c2ec980b3e8a967dd07b2c4c2ecfd702245
iwaag <eiji-of-empires@nifty.com>
2026-03-29 22:31:21 +0900

    fix

--- src/agpy/clients/db/sql.py
+++ src/agpy/clients/db/sql.py
@@ -3,8 +3,8 @@ from sqlmodel import create_engine
 from agpy.clients.db.config import get_database_settings
 
 def get_engine() -> Engine:
-    global _engine
-    if _engine is None:
-        _engine = create_engine(get_database_settings().url)
-    return _engine
+    global engine
+    if engine is None:
+        engine = create_engine(get_database_settings().url)
+    return engine
 engine: Engine | None = get_engine()
